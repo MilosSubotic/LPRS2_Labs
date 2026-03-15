ebe925ab817076f1f58a0eae5259837f3ad2aa08
Milos Subotic <milos.subotic.sm@gmail.com>
2026-03-16 00:37:59 +0100

    Test working.

--- FW/test__Ard_Nano_2_LPRS1_MAX1000_Shield/common.hpp
+++ FW/test__Ard_Nano_2_LPRS1_MAX1000_Shield/common.hpp
@@ -1,55 +1,131 @@
 
-#pragma
+#pragma once
 
-#include "../../Common/FW/avr_io_bitfields.h"
+#include "avr_io_bitfields.h"
+
+enum mux_color_or_segm_t {
+	RED = 0,
+	GREEN = 1,
+	BLUE = 2,
+	SEGM = 3
+};
 
 // TODO using PORTB, C, D
 struct bf_led {
+	// PINB
+	u8        : 8;
+	// DDRB
+	u8 odd_0  : 2;
+	u8        : 6;
+	// PORTB
+	struct {
+		u8 mux_color_or_segm : 2;
+		u8                   : 6;
+	};
+
+	// PINC
+	u8        : 8;
+	// DDRC
+	u8        : 1;
+	u8 odd_1  : 5;
+	u8        : 2;
+	// PORTC
+	union {
+		struct {
+			u8           : 1;
+			u8 n_segm_a  : 1;
+			u8 n_segm_b  : 1;
+			u8 n_segm_c  : 1;
+			u8 n_segm_d  : 1;
+			u8 n_segm_e  : 1;
+			u8           : 2;
+		};
+		struct {
+			u8           : 1;
+			u8 n_col_0   : 1;
+			u8 n_col_1   : 1;
+			u8 n_col_2   : 1;
+			u8 n_col_3   : 1;
+			u8 n_col_4   : 1;
+			u8           : 2;
+		};
+		struct {
+			u8           : 1;
+			u8 n_col_4_0 : 5;
+			u8           : 2;
+		};
+	};
+			
+
 	// PIND
 	u8        : 8;
 	// DDRD
-	u8        : 8;
+	u8        : 2;
+	u8 odd_2  : 6;
 	// PORTD
-	u8        : 8;
-
-	// PINC
-
-	// DDRC
-
-	// PORTC
-
-	// PINB
-	u8        : 8;
-
-	// DDRB
-
-	// PORTB
-
-	// PINA
-	u8        : 8;
-
-	// DDRA
-
-	// PORTA
 	union {
 		struct {
-			u8 r0 : 1;
-			u8 g0 : 1;
-			u8 b0 : 1;
-			u8 r1 : 1;
-			u8 g1 : 1;
-			u8 b1 : 1;
-			u8    : 2;
+			u8           : 2;
+			u8 n_segm_f  : 1;
+			u8 n_segm_g  : 1;
+			u8 n_segm_dp : 1;
+			u8           : 3;
 		};
 		struct {
-			u8 rgb0 : 3;
-			u8 rgb1 : 3;
-			u8      : 2;
+			u8           : 2;
+			u8 n_col_5   : 1;
+			u8 n_col_6   : 1;
+			u8 n_col_7   : 1;
+			u8           : 3;
+		};
+		struct {
+			u8           : 2;
+			u8 n_col_7_5 : 3;
+			u8           : 3;
+		};
+		struct {
+			u8           : 5;
+			u8 mux_row   : 3;
+		};
+		struct {
+			u8           : 5;
+			u8 mux_digit : 2;
+			u8           : 1;
 		};
 	};
 };
-#define led (*((volatile bf_led*)(&PIND)))
+#define led (*((volatile bf_led*)(&PINB)))
 
 struct bf_pb {
+	// PINB
+	u8        : 2;
+	u8 center : 1;
+	u8 right  : 1;
+	u8 up     : 1;
+	u8 left   : 1;
+	u8        : 2;
+	// DDRB
+	u8        : 2;
+	u8 idd_0  : 4;
+	u8        : 2;
+	// PORTB
+	u8        : 8;
+
+	// PINC
+	u8 down   : 1;
+	u8        : 7;
+	// DDRC
+	u8 idd_1  : 1;
+	u8        : 7;
 };
-#define pb (*((volatile bf_pb*)(&PIND)))
+#define pb (*((volatile bf_pb*)(&PINB)))
+
+
+#define DEBUG(x) \
+	do{ \
+		Serial.print(#x" = "); Serial.println(x); \
+	}while(0)
+#define DEBUG_HEX(x) \
+	do{ \
+		Serial.print(#x" = 0x"); Serial.println(x, HEX); \
+	}while(0)
--- FW/test__Ard_Nano_2_LPRS1_MAX1000_Shield/test__Ard_Nano_2_LPRS1_MAX1000_Shield.ino
+++ FW/test__Ard_Nano_2_LPRS1_MAX1000_Shield/test__Ard_Nano_2_LPRS1_MAX1000_Shield.ino
@@ -6,10 +6,81 @@ void setup() {
 	Serial.begin(115200);
 	Serial.println(__FILE__);
 
-	//TODO
+	led.odd_0 = wide_bool(DDR_OUT);
+	led.odd_1 = wide_bool(DDR_OUT);
+	led.odd_2 = wide_bool(DDR_OUT);
+
+	pb.idd_0 = wide_bool(DDR_IN);
+	pb.idd_1 = wide_bool(DDR_IN);
+	
+	led.n_col_0 = 1;
+	led.n_col_1 = 1;
+	led.n_col_2 = 1;
+	led.n_col_3 = 1;
+	led.n_col_4 = 1;
+	led.n_col_5 = 1;
+	led.n_col_6 = 1;
+	led.n_col_7 = 1;
 }
 
 
 void loop() {
-	//TODO
+	delay(1000/25); // [ms]
+
+	static u8 col = 0;
+	// turn off all
+	led.n_col_0 = 1;
+	led.n_col_1 = 1;
+	led.n_col_2 = 1;
+	led.n_col_3 = 1;
+	led.n_col_4 = 1;
+	led.n_col_5 = 1;
+	led.n_col_6 = 1;
+	led.n_col_7 = 1;
+	// set appropriate colon.
+	switch(col){
+	case 0:
+		led.n_col_0 = 0;
+		break;
+	case 1:
+		led.n_col_1 = 0;
+		break;
+	case 2:
+		led.n_col_2 = 0;
+		break;
+	case 3:
+		led.n_col_3 = 0;
+		break;
+	case 4:
+		led.n_col_4 = 0;
+		break;
+	case 5:
+		led.n_col_5 = 0;
+		break;
+	case 6:
+		led.n_col_6 = 0;
+		break;
+	case 7:
+		led.n_col_7 = 0;
+		break;
+	}
+
+	// advance
+	col++;
+	if(col == 8){
+		col = 0;
+		led.mux_row++;
+		if(led.mux_row == 0){
+			led.mux_color_or_segm++;
+			
+			DEBUG(pb.center);
+			DEBUG(pb.up);
+			DEBUG(pb.down);
+			DEBUG(pb.left);
+			DEBUG(pb.right);
+			Serial.println();	
+		}
+	}
+	
+
 }
