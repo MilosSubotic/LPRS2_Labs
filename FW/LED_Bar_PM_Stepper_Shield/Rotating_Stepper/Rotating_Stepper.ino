379445e0ea8eb23ee2e75a40cc70e9e26b387786
sarakr10 <sara.krsmanovic.03@gmail.com>
2026-03-31 16:18:53 +0200

    rotating stepper works fine

---
+++ FW/LED_Bar_PM_Stepper_Shield/Rotating_Stepper/Rotating_Stepper.ino
@@ -0,0 +1,17 @@
+#define DELAY_TIME 5
+
+void setup() {
+	//Serial.begin(115200);
+	DDRD = 0xff;
+}
+
+void loop() {
+
+	PORTD = 0b10001000;
+	for(u8 i=0 ; i<4; i++){
+		delay(DELAY_TIME);
+		PORTD >>= 1;
+		
+	}
+
+}
