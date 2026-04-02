882fec4d956c1f85f774f439c74efad320a5e10b
Milos Subotic <milos.subotic.sm@gmail.com>
2026-04-02 09:00:47 +0200

    FW example.

---
+++ FW/Telecommand_LPRS1_Shield/protocol.hpp
@@ -0,0 +1,38 @@
+
+#include "type_shorts.h"
+
+enum cmd_t {
+	SET_MATRIX,
+	SET_SEGM,
+	GET_PBS
+};
+
+#define MAGIC 0xbaba
+
+struct master_2_slave_pkg_t {
+	u16 magic;
+	u8 cmd;
+	union {
+		struct matrix {
+			u16 row   : 3;
+			u16 col   : 3;
+			u16 color : 3;
+		};
+		struct segm {
+			u8 num    : 4;
+			u8 digit  : 2;
+		};
+	};
+};
+
+struct slave_2_master_pkg_t {
+	u16 magic;
+	union {
+		struct pbs {
+			u8 up     : 1;
+			u8 right  : 1;
+			u8 left   : 1;
+			u8 down   : 1;
+		};
+	};
+};
